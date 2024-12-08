59ceecbd34a31c4bbb4d7f03a78111be05442c67
wozkki <sezali.muzakki.28@gmail.com>
2024-12-08 17:21:09 +0700

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 import "../styles/Home.css";
 const HomePage = () => {
   return (
     <div className="utama">
       <div className="kotak1"></div>
       <div className="kotak2"></div>
-      <img className="logo" src="src/assets/v13_29.png" alt="Logo Navbar" />
-      <div className="menu">
-        <button className="ikon"><a href="/"></a></button>
+      <img className="logo" src="/assets/v13_29.png" alt="Logo Navbar" />
+      <div className="menu"> 
+       <a href="/Register"> <button className="ikon"  >Register
+        </button></a>
       </div>
-      <div className="cari">
-        <div className="ikon"></div>
-      </div>
-      <img className="gambar-utama" src="src/assets/v13_32.png" alt="Gambar Utama" />
+      <img
+        className="gambar-utama"
+        src="/assets/v13_32.png"
+        alt="Gambar Utama"
+      />
       <div className="nano-diane">Nano Diane</div>
       <div className="wanita">WANITA</div>
-      <div className="jelajahi-digital">Jelajahi Peluncuran Digital Eksklusif</div>
+      <div className="jelajahi-digital">
+        Jelajahi Peluncuran Digital Eksklusif
+      </div>
       <div className="garis"></div>
-      <div className="avatar">
-        <div className="placeholder-avatar"></div>
+      <div className="avatar"><a href="/Login">Login</a>
       </div>
     </div>
   );
